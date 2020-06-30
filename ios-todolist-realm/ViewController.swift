bafee9d92aa244bf961e76ec29799fd50638ae49
omrobbie <putu.roby@yahoo.com>
2020-06-30 09:48:51 +0800

    write data

--- ios-todolist-realm/ViewController.swift
+++ ios-todolist-realm/ViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import RealmSwift
+
+// For Realm object, we have to use class instead of struct.
+class Item: Object  {
+
+    @objc dynamic var title: String = ""
+    @objc dynamic var status: Bool = false
+}
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    private let realm = try! Realm()
+    private lazy var items: Results<Item> = {
+        realm.objects(Item.self)
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Get file location
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+
         setupList()
     }
 
@@ -23,13 +40,29 @@ class ViewController: UIViewController {
         tableView.dataSource = self
     }
 
+    private func saveData(_ item: Item) {
+        do {
+            try realm.write {
+                realm.add(item)
+            }
+        } catch {
+            print(error.localizedDescription)
+            return
+        }
+    }
+
     @IBAction func btnAddTapped(_ sender: Any) {
         var textField = UITextField()
 
         let alertVC = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
         let actionCancel = UIAlertAction(title: "Cancel", style: .cancel)
         let actionAdd = UIAlertAction(title: "Add", style: .default) { (_) in
-            print(textField.text!)
+            let item = Item()
+            item.title = textField.text!
+            item.status = false
+
+            self.saveData(item)
+            self.tableView.reloadData()
         }
 
         alertVC.addTextField { (alertTextField) in
@@ -47,12 +80,14 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        cell.textLabel?.text = "Item \(indexPath.row)"
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.status ? .checkmark : .none
         return cell
     }
 }
